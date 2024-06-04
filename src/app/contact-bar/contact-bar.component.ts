d6641018ef17820474ed15cc930e8dfec260eaec
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-04 23:40:00 +0100

    add Contact bar component

---
+++ src/app/contact-bar/contact-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-bar',
+  templateUrl: './contact-bar.component.html',
+  styleUrl: './contact-bar.component.css'
+})
+export class ContactBarComponent {
+
+}
