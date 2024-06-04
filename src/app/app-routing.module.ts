3f49f47b32c9c84d790b4aef8a6b5b048c252414
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-04 23:55:40 +0100

    add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { TarifPageComponent } from './tarif-page/tarif-page.component';
+import { ServicePageComponent } from './service-page/service-page.component';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent},
+  { path: "signup", component: SignUpComponent},
+  { path: "home", component: HomeComponent},
+  { path: "tarif", component: TarifPageComponent},
+  { path: "service", component: ServicePageComponent},
+  { path: "contact", component: ContactUsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarHomeComponent } from './nav-bar-home/nav-bar-home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactBarComponent } from './contact-bar/contact-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { TarifPageComponent } from './tarif-page/tarif-page.component';
+import { ServicePageComponent } from './service-page/service-page.component';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarHomeComponent,
+    NavBarComponent,
+    SideBarComponent,
+    FooterComponent,
+    ContactBarComponent,
+    HomeComponent,
+    TarifPageComponent,
+    ServicePageComponent,
+    ContactUsPageComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [
     provideClientHydration()
