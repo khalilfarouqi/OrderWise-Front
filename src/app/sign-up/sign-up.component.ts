5a938dbe68f2d663cb9c7307e30db1c4ac2f001d
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-05 22:47:01 +0100

    finish page of sign up component

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      userType: new FormControl('', Validators.required)
+    });
+  }
+
+  submitForm(): void {
+    console.log('Form submitted:', this.contactForm.value);
+    this.contactForm.value.username = this.contactForm.value.firstname + '.' + this.contactForm.value.lastname;
+    this.contactForm.value.userType = 'NEW_USER';
+    this.userService.postUserForm(this.contactForm.value).subscribe({
+      next: (response) => {
+        this.contactForm.reset();
+      },
+      error: (error) => {
+        console.error('Error  ==>  ', error);
+      }
+    });
+  }
 
 }
