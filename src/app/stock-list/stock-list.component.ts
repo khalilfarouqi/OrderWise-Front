c603dc5059c8eeba18b471bc779a2c733a988ee0
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-06 14:23:22 +0100

    add stock list component

---
+++ src/app/stock-list/stock-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-list',
+  templateUrl: './stock-list.component.html',
+  styleUrl: './stock-list.component.css'
+})
+export class StockListComponent {
+
+}
