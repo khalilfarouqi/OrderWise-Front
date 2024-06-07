16f146f8f6519c7c8ad0ff1829ab965699d75f3f
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-07 22:39:47 +0100

    Add product template and methods

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private productService: ProductService) { }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getProducts() {
+    this.productService.getProductUrl().subscribe(
+      (data) => {
+        console.log(data);
+        this.products = data;
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 
 }
