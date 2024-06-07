009af3fcb5a4c7ab543affe41d98281a1e8c9135
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-07 22:58:00 +0100

    add stock template and ts

--- src/app/stock-list/stock-list.component.ts
+++ src/app/stock-list/stock-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from '../service/stock.service';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
   styleUrl: './stock-list.component.css'
 })
-export class StockListComponent {
+export class StockListComponent implements OnInit {
+  stocks: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private stockService: StockService) { }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
+  
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.stockService.getStockUrl().subscribe(
+      (data) => {
+        console.log(data);
+        this.stocks = data;
+      },
+      (error) => {
+        console.error('Error fetching stocks:', error);
+      }
+    );
+  }
 
 }
