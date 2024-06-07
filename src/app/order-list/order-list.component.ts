342b517846dde3445ac6c659437262d9e9f60454
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-07 22:39:31 +0100

    Add order template and methodes

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../service/order.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.css'
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
+  orders: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private orderService: OrderService) { }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
+  
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrderUrl().subscribe(
+      (data) => {
+        console.log(data);
+        this.orders = data;
+      },
+      (error) => {
+        console.error('Error fetching orders:', error);
+      }
+    );
+  }
 
 }
