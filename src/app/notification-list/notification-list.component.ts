210367284f0c8b733f4c0c0cffa4c666a1bd8e70
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-08 14:51:50 +0100

    add notification template

--- src/app/notification-list/notification-list.component.ts
+++ src/app/notification-list/notification-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../service/notification.service';
 
 @Component({
   selector: 'app-notification-list',
   templateUrl: './notification-list.component.html',
   styleUrl: './notification-list.component.css'
 })
-export class NotificationListComponent {
+export class NotificationListComponent implements OnInit {
+  notifications: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private notificationService: NotificationService) { }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
+  
+  ngOnInit(): void {
+    this.getNotification();
+  }
+  
+  removeNotification(index: number): void {
+    this.notifications.splice(index, 1);
+  }
+
+  getNotification() {
+    this.notificationService.getNotificationUrl().subscribe(
+      (data) => {
+        console.log(data);
+        this.notifications = data;
+      },
+      (error) => {
+        console.error('Error fetching notifications:', error);
+      }
+    );
+  }
 
 }
