90d0b9ade991240c9dc765725589ceb4ad63a2ab
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-09 22:24:25 +0100

    add setting template

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css'
 })
-export class SettingComponent {
+export class SettingComponent implements OnInit {
+  prodile: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    //this.getProfile('khalil.farouqi');
+  }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
+
+  getProfile(username: string) {
+    this.userService.getProfile(username).subscribe(
+      (data) => {
+        console.log(data);
+        this.prodile = data;
+      },
+      (error) => {
+        console.error('Error fetching prodile : ', error);
+      }
+    );
+  }
 
 }
