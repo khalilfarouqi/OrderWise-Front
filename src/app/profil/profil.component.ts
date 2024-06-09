ccf70dc02302eb39e408324bae66a3b86e2444f6
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-09 22:24:51 +0100

    add much profil page with sidebar

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './profil.component.css'
 })
 export class ProfilComponent {
+  prodile: any[] = [];
+  isSidebarOpen = true;
+
+  constructor(private userService: UserService) { }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
 
 }
