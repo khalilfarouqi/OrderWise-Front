33c1037912240fa05f7d4d89371a34a25e0ee5d7
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-10 23:19:56 +0100

    upgrade dashboard template

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { OrderService } from '../service/order.service';
+import { isPlatformBrowser } from '@angular/common';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  period: string = 'monthly';
+  isSidebarOpen = true;
+
+  constructor(private orderService:OrderService) { }
+
+  ngOnInit(): void {
+    this.getStatic();
+  }
+
+  getStatic(){
+
+  }
+
+  setPeriod(period: string) {
+    this.period = period;
+  }
+
+  onSidebarToggled(isOpen: boolean) {
+    this.isSidebarOpen = isOpen;
+  }
 }
