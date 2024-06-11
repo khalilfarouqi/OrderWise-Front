30c17fe2450c85a03a5abf71cf6372719582026b
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-11 23:48:15 +0100

    add user interface

---
+++ src/app/models/User.ts
@@ -0,0 +1,22 @@
+import { City } from "../enum/city.enum";
+import { Gender } from "../enum/gender.enum";
+import { Role } from "../enum/role.enum";
+import { UserType } from "../enum/userType.enum";
+
+export interface User {
+    id: Number;
+    lastCheckIn: string;
+    firstname: string;
+    lastname: string;
+    username: string;
+    email: string;
+    password: string;
+    cin: string;
+    tel: string;
+    image: string;
+
+    city: City;
+    gender: Gender;
+    role: Role;
+    userType: UserType;
+}
