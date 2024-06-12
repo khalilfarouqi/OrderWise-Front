6ebfbc37c17f64ab5d07e9db5e31fe82a50186af
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-12 23:31:42 +0100

    show user data in setting comp

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
+import { City } from '../enum/city.enum';
+import { User } from '../models/User';
+import { Gender } from '../enum/gender.enum';
+import { Role } from '../enum/role.enum';
+import { UserType } from '../enum/userType.enum';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-setting',
@@ -7,13 +13,36 @@ import { UserService } from '../service/user.service';
   styleUrl: './setting.component.css'
 })
 export class SettingComponent implements OnInit {
-  prodile: any[] = [];
+
+  userForm!: FormGroup;
+  profile: User = {
+    username: '',
+    id: 0,
+    lastCheckIn: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    cin: '',
+    tel: '',
+    image: '',
+    city: City.AGOURI_MEKNES,
+    gender: Gender.MALE,
+    role: Role.ADMIN,
+    userType: UserType.ADMIN
+  };
+
   isSidebarOpen = true;
+  cities = Object.values(City);
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    //this.getProfile('khalil.farouqi');
+    this.getProfile('khalil.farouqi');
+    this.userForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required)
+    });
   }
 
   onSidebarToggled(isOpen: boolean) {
@@ -23,13 +52,25 @@ export class SettingComponent implements OnInit {
   getProfile(username: string) {
     this.userService.getProfile(username).subscribe(
       (data) => {
-        console.log(data);
-        this.prodile = data;
+        this.profile = data;
       },
       (error) => {
-        console.error('Error fetching prodile : ', error);
+        console.error('Error fetching profile : ', error);
       }
     );
+  }
+
+  submitForm(): void {
+    console.log('Form submitted:', this.userForm.value);
+    this.userService.updateUserForm(this.userForm.value).subscribe({
+      next: (response) => {
+        console.log("response  --->  " + response)
+        this.userForm.reset();
+      },
+      error: (error) => {
+        console.error('Error  ==>  ', error);
+      }
+    });
   }
 
 }
