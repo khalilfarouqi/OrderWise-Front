135df182b128a27ae5572e0ee6b80ab806af1772
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-02 21:53:07 +0100

    clean code

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../service/user.service';
 import { User } from '../models/User';
 import { MatDialog } from '@angular/material/dialog';
 import { ProfileDialogComponent } from '../profile-dialog/profile-dialog.component';
+import Swal from 'sweetalert2';
+import { UserType } from '../enum/userType.enum';
 
 @Component({
   selector: 'app-profil',
@@ -12,6 +14,8 @@ import { ProfileDialogComponent } from '../profile-dialog/profile-dialog.compone
 export class ProfilComponent implements OnInit {
   profiles: User[] = [];
   isSidebarOpen = true;
+
+  selectedProfile: any = null;
 
   constructor(private userService: UserService, public dialog: MatDialog) { }
 
@@ -34,9 +38,83 @@ export class ProfilComponent implements OnInit {
     );
   }
 
-  openDialog(profile: User): void {
-    this.dialog.open(ProfileDialogComponent, {
-      data: profile
+  openModal(profile: User): void {
+    const dialogRef = this.dialog.open(ProfileDialogComponent, {
+      width: '600px',
+      data: { profile }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (result.userType == UserType.SELLER) {
+          this.valide(result);
+        } else if (result.userType == UserType.REFUSER) {
+          this.refuse(result);
+        }
+      }
+    });
+  }
+
+  updateProfile(updatedProfile: User) {
+    const index = this.profiles.findIndex(profile => profile.id === updatedProfile.id);
+    if (index !== -1) {
+      this.profiles[index] = updatedProfile;
+      // Here, you can call a service method to save the updated profile to the backend if needed
+      this.userService.updateUserPopupForm(updatedProfile).subscribe(
+        response => {
+          console.log('Profile updated successfully', response);
+          this.showAlert('Profile updated successfully', response, 'success');
+        },
+        error => {
+          console.error('Error updating profile', error);
+          this.showAlert('Error updating profile', error, 'error');
+        }
+      );
+    }
+  }
+
+  refuse(updatedProfile: User) {
+    const index = this.profiles.findIndex(profile => profile.id === updatedProfile.id);
+    if (index !== -1) {
+      this.profiles[index] = updatedProfile;
+      // Here, you can call a service method to save the updated profile to the backend if needed
+      this.userService.refuseUrl(updatedProfile).subscribe(
+        response => {
+          console.log('Profile refuse successfully', response);
+          this.showAlert('Profile refuse successfully', response, 'success');
+        },
+        error => {
+          console.error('Error refuse profile', error);
+          this.showAlert('Error refuse profile', error.error.message, 'error');
+        }
+      );
+    }
+  }
+
+  valide(updatedProfile: User) {
+    const index = this.profiles.findIndex(profile => profile.id === updatedProfile.id);
+    if (index !== -1) {
+      this.profiles[index] = updatedProfile;
+      // Here, you can call a service method to save the updated profile to the backend if needed
+      this.userService.valideUrl(updatedProfile).subscribe(
+        response => {
+          console.log('Profile is valide successfully', response);
+          this.showAlert('Profile is valide successfully', response, 'success');
+        },
+        error => {
+          console.error('Error in valide profile', error);
+          this.showAlert('Error in valide profile', error.error.message, 'error');
+        }
+      );
+    }
+  }
+
+  showAlert(title: string, text: string, icon: any) {
+    Swal.fire({
+      title: title,
+      text: text,
+      icon: icon,
+      confirmButtonText: 'OK'
     });
   }
 
