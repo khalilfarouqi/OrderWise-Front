bca8575af8f4d581b2f677c71ed8b4279b855223
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-02 21:53:30 +0100

    Add popup of valide user

--- src/app/profile-dialog/profile-dialog.component.ts
+++ src/app/profile-dialog/profile-dialog.component.ts
@@ -1,5 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { City } from '../enum/city.enum';
+import { Gender } from '../enum/gender.enum';
+import { User } from '../models/User';
+import { UserType } from '../enum/userType.enum';
 
 @Component({
   selector: 'app-profile-dialog',
@@ -7,9 +11,37 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrl: './profile-dialog.component.css'
 })
 export class ProfileDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<ProfileDialogComponent>) { }
+  profile: User;
 
-  onClose(): void {
+  cities: { key: string, value: string }[] = [];
+  genders: { key: string, value: string }[] = []; 
+
+  constructor(
+    public dialogRef: MatDialogRef<ProfileDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.profile = { ...data.profile };
+    this.cities = Object.entries(City).map(([key, value]) => ({ key, value }));
+    this.genders = Object.entries(Gender).map(([key, value]) => ({ key, value }));
+  }
+
+  onSave(): void {
+    this.dialogRef.close(this.profile);
+  }
+
+  onCancel(): void {
     this.dialogRef.close();
+  }
+
+  onValide(): void {
+    console.log(this.profile)
+    this.profile.userType = UserType.SELLER;
+    console.log(this.profile)
+    this.dialogRef.close(this.profile);
+  }
+
+  onRefuser(): void {
+    this.profile.userType = UserType.REFUSER;
+    this.dialogRef.close(this.profile);
   }
 }
