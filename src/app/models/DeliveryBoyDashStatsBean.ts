38223791827687a7d55c0994a46522c89d6e0ef2
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-03 20:36:26 +0100

    Add Delivery Boy Dash Stats Bean

---
+++ src/app/models/DeliveryBoyDashStatsBean.ts
@@ -0,0 +1,17 @@
+export interface DeliveryBoyDashStatsBean {
+    walletToday: number;
+    oneyDepose: number;
+    oneyPacket: number;
+    orderTreatedToday: number;
+    orderNoTreatedToday: number;
+    orderNotTreated: number;
+    orderLivrer: number;
+    orderAnnuler: number;
+    orderNotResponse: number;
+    orderToTraite: number;
+    dateOrderToTraite: number;
+    orderToDeliver: number;
+    dateOrderToDeliver: number;
+    orderToReturn: number;
+    dateOrderToReturn: number;
+}
