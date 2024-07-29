8470682c9510f4a8c48c561bba7728a6eda596f3
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-29 14:59:08 +0100

    Add ConfigApp model

---
+++ src/app/models/configApp.ts
@@ -0,0 +1,15 @@
+export interface ConfigApp {
+    id: number;
+    email: String;
+    tel: String;
+    address: String;
+    facebookUrl: String;
+    instagramUrl: String;
+    whatsappUrl: String;
+    twitterUrl: String;
+    linkedInUrl: String;
+    youtubeUrl: String;
+
+    avgDelivered: String;
+    avgConfirmation: String;
+}
