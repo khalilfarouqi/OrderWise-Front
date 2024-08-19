7544214f6c2e82a5babc850fbe82e83a4ed72536
khalilfarouqi <bio.khalil.f@gmail.com>
2024-08-19 15:59:22 +0100

    Set configuration of app in footer and contact bat

--- src/app/contact-bar/contact-bar.component.ts
+++ src/app/contact-bar/contact-bar.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigApp } from '../models/configApp';
+import { ConfigAppService } from '../service/config-app.service';
 
 @Component({
   selector: 'app-contact-bar',
   templateUrl: './contact-bar.component.html',
   styleUrl: './contact-bar.component.css'
 })
-export class ContactBarComponent {
+export class ContactBarComponent implements OnInit {
+  
+  configApp: ConfigApp = {
+    id: 0,
+    email: '',
+    tel: '',
+    address: '',
+    facebookUrl: '',
+    instagramUrl: '',
+    whatsappUrl: '',
+    twitterUrl: '',
+    linkedInUrl: '',
+    youtubeUrl: '',
+
+    avgDelivered: '',
+    avgConfirmation: ''
+  };
+
+  constructor(private configAppService: ConfigAppService) {}
+
+  ngOnInit(): void {
+    this.getConfigApp();
+  }
+
+  getConfigApp() {
+    this.configAppService.getConfigAppUrl().subscribe(
+      (data) => {
+        this.configApp = data;
+      },
+      (error) => {
+        console.error('Error fetching config app : ', error);
+      }
+    );
+  }
 
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigApp } from '../models/configApp';
+import { ConfigAppService } from '../service/config-app.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  
+  configApp: ConfigApp = {
+    id: 0,
+    email: '',
+    tel: '',
+    address: '',
+    facebookUrl: '',
+    instagramUrl: '',
+    whatsappUrl: '',
+    twitterUrl: '',
+    linkedInUrl: '',
+    youtubeUrl: '',
 
+    avgDelivered: '',
+    avgConfirmation: ''
+  };
+
+  constructor(private configAppService: ConfigAppService) {}
+
+  ngOnInit(): void {
+    this.getConfigApp();
+  }
+
+  getConfigApp() {
+    this.configAppService.getConfigAppUrl().subscribe(
+      (data) => {
+        this.configApp = data;
+      },
+      (error) => {
+        console.error('Error fetching config app : ', error);
+      }
+    );
+  }
 }
